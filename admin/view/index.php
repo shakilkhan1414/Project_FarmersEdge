6cb12a2d06c0811b67c32ef99d9735031343c740
MD Shakil Khan <khan.shakil.1414@gmail.com>
2021-04-09 22:34:59 +0600

    Add files via upload

--- admin/view/index.php
+++ admin/view/index.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
-<!-- Created By CodingNepal -->
-<html lang="en" dir="ltr">
+<html>
   <head>
-    <meta charset="utf-8">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   </head>
   <body>
-    <div class="content">
-      <div class="text">
-Login Form</div>
-<form action="#">
-        <div class="field">
-          <input type="text" required>
-          <span class="fas fa-user"></span>
-          <label>Email or Phone</label>
+    <section id="login_wrapper">
+        <div class="content">
+            <div class="text">FarmersEdge-Admin</div>
+            <span class="error">*Invalid email or password!</span>
+                <form action="#">
+                        <div class="field">
+                            <input type="text" required>
+                            <span class="fas fa-user"></span>
+                            <label>Email or Phone</label>
+                        </div>
+                        <div class="field">
+                            <input type="password" required>
+                            <span class="fas fa-lock"></span>
+                            <label>Password</label>
+                        </div>
+                        <div class="forgot-pass">
+                            <a href="#">Forgot Password?</a>
+                        </div>
+                            <button>Sign in</button>
+                </form>
         </div>
-<div class="field">
-          <input type="password" required>
-          <span class="fas fa-lock"></span>
-          <label>Password</label>
-        </div>
-<div class="forgot-pass">
-<a href="#">Forgot Password?</a></div>
-<button>Sign in</button>
-        <div class="sign-up">
-Not a member?
-          <a href="#">signup now</a>
-        </div>
-</form>
-</div>
+</section>
 </body>
 </html>
